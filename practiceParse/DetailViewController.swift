6851597423a97749f7a4442f9d7f9c2da6885a9d
MoreNiceJay <morenicejay@gmail.com>
2015-11-05 17:55:29 -0500

    리치어빌리티 시작

--- practiceParse/DetailViewController.swift
+++ practiceParse/DetailViewController.swift
@@ -12,7 +12,7 @@ import Parse
 class DetailViewController: UIViewController {
     var object : PFObject?
     
-    @IBOutlet weak var texField: UITextField!
+    @IBOutlet weak var textField: UITextField!
 
     @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
@@ -52,30 +52,28 @@ class DetailViewController: UIViewController {
     
     @IBAction func editButtonTapped(sender: AnyObject) {
     
-    var query = PFQuery(className:"Practice")
-    query.getObjectInBackgroundWithId((object?.objectId)!) {
-    (obj: PFObject?, error: NSError?) -> Void in
-    if error != nil {
-    print(error)
-    } else if let obj = obj {
-    obj["text"] = self.texField.text
-    
-    obj.saveInBackgroundWithBlock({ (success, error) -> Void in
-        if error == nil{
-            let myAlert = UIAlertController(title: "성공", message: "업데아트 완료", preferredStyle: UIAlertControllerStyle.Alert)
-            
-            let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
-            myAlert.addAction(okAction)
-            self.presentViewController(myAlert, animated: true, completion: {Void in
-            
-            self.performSegueWithIdentifier("detailToMain", sender: self)})
-
-            
+    let query = PFQuery(className: "Practice")
+        query.getObjectInBackgroundWithId((object?.objectId)!) { (obj, error) -> Void in
+            if error != nil {
+                    print(error)
+            }else if let obj = obj {
+                
+                obj["text"] = self.textField.text
+                
+                
+                obj.saveInBackgroundWithBlock({ (success, error) -> Void in
+                    if error == nil {
+                        self.performSegueWithIdentifier("detailToMain", sender: self)
+                        
+                        
+                    }
+                })
+            }
         }
-    })
-    }
-    }
+    
     }
     
-
-}
+    
+    
+    
+  }
